5149adce768a8207233492f12ada3f1b99159f1a
restuser <restuser@UbuntuVM.(none)>
2019-04-06 15:44:59 +0200

    Implementado el servidor

---
+++ src/clase/resources/BBDD/Database.java
@@ -0,0 +1,36 @@
+package clase.resources.BBDD;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import org.apache.naming.NamingContext;
+@Path("Hol")
+public class Database {
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayPlain() {
+		return "MAX LA CHUPA";
+	}
+	
+}
