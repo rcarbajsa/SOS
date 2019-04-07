a91e04ae08475df16c228895ce22b4bbf45b2550
onmax <maximogarciamtnez@gmail.com>
2019-04-07 11:54:12 +0200

    sql script

---
+++ mysql/sql-scripts/create-tables.sql
@@ -0,0 +1,40 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+
+CREATE TABLE IF NOT EXISTS `chats` (
+  `chat_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `from_user_id` int(10) unsigned NOT NULL,
+  `to_user_id` int(10) unsigned NOT NULL,
+  `content` text NOT NULL,
+  PRIMARY KEY (`chat_id`),
+  FOREIGN KEY (`from_user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (`to_user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+
+CREATE TABLE IF NOT EXISTS `friends` (
+  `user_id` int(10) unsigned NOT NULL,
+  `friend_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`user_id`,`friend_id`),
+  KEY `FK_FRIENDS_2` (`friend_id`),
+  CONSTRAINT `FK_FRIENDS_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `FK_FRIENDS_2` FOREIGN KEY (`friend_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE IF NOT EXISTS `posts` (
+  `post_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `content` text NOT NULL,
+  `createdAt` TIMESTAMP NOT NULL DEFAULT NOW(),
+  `updated_at` TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+  PRIMARY KEY (`post_id`),
+  KEY `users_posts_FK` (`user_id`),
+  CONSTRAINT `users_posts_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+    ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
---
+++ mysql/sql-scripts/insert-data.sql
@@ -0,0 +1,17 @@
+use faceSOS;
+
+INSERT INTO users
+    (name, username) VALUE
+    ('Alejandro', 'alexente');
+
+INSERT INTO users
+    (name, username) VALUE
+    ('Raúl', 'rcarbjsa');
+
+INSERT INTO users
+    (name, username) VALUE
+    ('Max', 'onmax');
+
+INSERT INTO users
+    (name, username) VALUE
+    ('Gema', 'gemawari');
