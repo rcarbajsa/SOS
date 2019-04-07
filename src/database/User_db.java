b878f47cc986c71da8e0160284557a41c18da7a6
restuser <restuser@UbuntuVM.(none)>
2019-04-07 12:38:30 +0200

    Implementada conexion a la db

--- src/database/User_db.java
+++ src/database/User_db.java
@@ -1,6 +1,9 @@
 package database;
 import resources.*;
-public class User_db {
+public class User_db extends Conexion{
+	public User_db() {
+		super();
+	}
 	public String createUser(User_resource user) {
 		return "location";
 	}
--- src/resources/User_resource.java
+++ src/resources/User_resource.java
@@ -8,15 +8,11 @@ public class User_resource {
 	private int id;
 	private String username;
 	private String name;
-	private String surname1;
-	private String surname2;
 	private ArrayList<Post> posts;
 	private ArrayList<User_resource> user_resources;
 	
-	public User_resource(String username, String name, String surname1,String surname2) {
+	public User_resource(String username, String name) {
 		this.username=username;
-		this.surname1=surname1;
-		this.surname2=surname2;
 		this.name=name;
 		this.posts=new ArrayList<Post>();
 		this.user_resources=new ArrayList<User_resource>();
