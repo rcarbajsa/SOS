b1ecad2ecc14c200ef12fdee47a06e6c7efcf701
onmax <maximogarciamtnez@gmail.com>
2019-04-08 11:55:49 +0200

    modify tables

--- mysql/sql-scripts/create-tables.sql
+++ mysql/sql-scripts/create-tables.sql
@@ -1,3 +1,4 @@
+use faceSOS;
 CREATE TABLE IF NOT EXISTS `users` (
   `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(32) NOT NULL,
@@ -8,12 +9,13 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 CREATE TABLE IF NOT EXISTS `chats` (
   `chat_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `from_user_id` int(10) unsigned NOT NULL,
-  `to_user_id` int(10) unsigned NOT NULL,
+  `sender_user_id` int(10) unsigned NOT NULL,
+  `receiver_user_id` int(10) unsigned NOT NULL,
   `content` text NOT NULL,
+  `sent_at` TIMESTAMP NOT NULL DEFAULT NOW(),
   PRIMARY KEY (`chat_id`),
-  FOREIGN KEY (`from_user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (`to_user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (`sender_user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (`receiver_user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
 
@@ -31,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `post_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned NOT NULL,
   `content` text NOT NULL,
-  `createdAt` TIMESTAMP NOT NULL DEFAULT NOW(),
+  `created_at` TIMESTAMP NOT NULL DEFAULT NOW(),
   `updated_at` TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
   PRIMARY KEY (`post_id`),
   KEY `users_posts_FK` (`user_id`),
